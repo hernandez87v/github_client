124182046332bd26c468ac54bfef5b37fc800e55
Vlad <hernandez87v@hotmail.com>
2022-02-10 16:25:51 -0800

    change to grey accent

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GitHub Client',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'GitHub Client'),
